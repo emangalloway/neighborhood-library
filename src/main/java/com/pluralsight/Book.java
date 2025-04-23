cd260779a065a6fd8ec0fc3c3dbd569a1eb6b259
emangalloway <emanuelgalloway25@gmail.com>
2025-04-23 08:53:18 -0400

    Created Book class and a static array in the NeighborhoodLibrary project

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,64 @@
 package com.pluralsight;
 
 public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String isCheckedOutTo;
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String isCheckedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.isCheckedOutTo = isCheckedOutTo;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getIsCheckedOutTo() {
+        return isCheckedOutTo;
+    }
+
+    public void setIsCheckedOutTo(String isCheckedOutTo) {
+        this.isCheckedOutTo = isCheckedOutTo;
+    }
+    public void checkOut(String name){
+        System.out.println(name+ ", Enjoy reading "+title+"!");
+    }
+    public void checkIn(){
+        System.out.println("You have successfully returned "+title);
+    }
 }
+
+
--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,7 +1,24 @@
 package com.pluralsight;
 
 public class NeighborhoodLibrary {
+    //Initialize an array of Book objects with a max capacity of 20
+    private static Book[] books = new Book[20];
+
+    // The number of books that are currently in the library
+    private static int numOfBooks = 5;
+
+    //Main method where the program will run
     public static void main(String[] args) {
-        
+
+        //Create and add books to array
+        books[0] = new Book(1234, "1a2b3c", "Heidi", false, "");
+        books[1] = new Book(3367, "2b3c4d", "The Lion, the Witch, and the Wardrobe", false, "");
+        books[2] = new Book(2988, "3c4d5e", "Harry Potter, and the Chamber of Secrets", false, "");
+        books[3] = new Book(1244, "4d5e6f", "The Hobbit", false, "");
+        books[4] = new Book(5674, "5e7f8g", "A Tale of Two Cities", false, "");
+
+
+
+
     }
 }
