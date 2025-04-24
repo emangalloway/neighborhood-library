b76156e5f8892008956281b5bfc1bdb5b14c78e3
emangalloway <emanuelgalloway25@gmail.com>
2025-04-23 20:03:46 -0400

    Created scanner, also created a while loop for the menue options

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class NeighborhoodLibrary {
     //Initialize an array of Book objects with a max capacity of 20
     private static Book[] books = new Book[20];
@@ -16,6 +18,24 @@ public class NeighborhoodLibrary {
         books[2] = new Book(2988, "3c4d5e", "Harry Potter, and the Chamber of Secrets", false, "");
         books[3] = new Book(1244, "4d5e6f", "The Hobbit", false, "");
         books[4] = new Book(5674, "5e7f8g", "A Tale of Two Cities", false, "");
+
+        //Create a scanner
+        Scanner myscanner = new Scanner(System.in);
+
+        //Create a while loop for the menu
+        while (true){
+            System.out.println("Welcome to the Library");
+            System.out.println("1 - Library Home Screen");
+            System.out.println("2 - Show available books");
+            System.out.println("3 - Show checked out books");
+            System.out.println("4 - Check in a book");
+            System.out.println("5 - Quit");
+            System.out.println("Enter option: ");
+            int enterOption = myscanner.nextInt();
+            
+
+
+        }
 
 
 
